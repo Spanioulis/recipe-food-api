334f3dbcebc92dfa993c67719d5e06df51010677
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-04-04 15:44:40 +0200

    style: add GlobalStyle from styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css';
+import { GlobalStyle } from './styles';
 import { useFetch } from './utils/useFetch';
 
-console.log('API_KEY:', import.meta.env.VITE_API_KEY);
 const ingredient = 'fish';
 
 function App() {
    const URL = `https://api.spoonacular.com/recipes/search?query=${ingredient}&number=20&apiKey=${
       import.meta.env.VITE_API_KEY
    }`;
-   const { data } = useFetch(URL);
-   console.log('data:', data);
+   // const { data } = useFetch(URL);
+   // console.log('data:', data);
    return (
-      <div className="App">
-         <h1>Spoonacular - Recipe&Food 🥗🍤🍍🥑🍄</h1>
-      </div>
+      <>
+         <GlobalStyle />
+         <div className="App">
+            <h1>Spoonacular - Recipe&Food 🥗🍤🍍🥑🍄</h1>
+         </div>
+      </>
    );
 }
 
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,4 @@
-export { dimensions } from './dimensions';
 export { colors } from './colors';
+export { dimensions } from './dimensions';
 export { FlexBox } from './FlexBox';
+export { GlobalStyle } from './GlobalStyle';
